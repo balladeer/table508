6478e459da5033a608171e9c7cb7491362db7360
Zachary T Cross <zachary.t.cross@gmail.com>
2018-04-01 11:06:12 -0400

    Added `rownames_as_headers` support.

--- R/create_508_table.R
+++ R/create_508_table.R
@@ -1,13 +1,28 @@
 #' Create a 508-compliant HTML table from tabular data
 #'
-#' \code{create_508_table} returns an HTML string representing a 508-compliant version of the tabular data \code{.data}.
-#'
-#' @examples
-#' df <- tibble('location'=c('Denver', 'DC', 'Miami'), 'meal'= c('dinner', 'lunch', 'lunch'))
-#' html_text <- create_508_table(df)
+#' \code{create_508_table} returns an HTML string representing a 508-compliant
+#' version of the tabular data \code{.data}.
+#' @param .data A data frame
+#' @param table_id A string representing the \code{id} attribute of the
+#'   resulting \code{<table>} node
+#' @param rownames_as_headers A boolean toggling whether to include row headers
+#'   based on the \code{rownames(.data)}
+#' @param top_left_cell A string indicating value of top-left cell; used when
+#'   rownames_as_headers=T
+#' @return xml2 node containing \code{<table>}
 #'
 #' @export
-create_508_table <- function(.data, table_id = "table"){
+create_508_table <- function(.data, table_id = "table",
+                             rownames_as_headers=FALSE, top_left_cell=NULL){
+
+  # Validate arguments
+  if (rownames_as_headers && is.null(top_left_cell)){
+    stop('Can not use rownames_as_headers without setting top_left_cell as well')
+  }
+
+  COL_ID_TEMPLATE = "columnHead%i"
+  ROW_ID_TEMPLATE = "rowHead%i"
+
   # Create the root xml node
   r <- xml2::xml_new_root("root")
 
@@ -18,20 +33,30 @@ create_508_table <- function(.data, table_id = "table"){
   header <- xml2::xml_add_child(table, "thead")
   header_row <- xml2::xml_add_child(header, "tr")
 
+  if (rownames_as_headers){
+    xml2::xml_add_child(header_row, "th", top_left_cell, id=sprintf(COL_ID_TEMPLATE, 0))
+  }
   for (i in seq_along(colnames(.data))){
-    element_id <- sprintf("columnHead%i", i)
+    element_id <- sprintf(COL_ID_TEMPLATE, i)
     xml2::xml_add_child(header_row, "th", colnames(.data)[i], id = element_id)
   }
 
   # Create each data row
   body <- xml2::xml_add_child(table, "tbody")
 
+  current_row_header <- NULL
   for (i in seq_len(nrow(.data))){
     xml_row <- xml2::xml_add_child(body, "tr")
 
+    if (rownames_as_headers){
+      row_header_id <- sprintf(ROW_ID_TEMPLATE, i)
+      xml2::xml_add_child(xml_row, 'th', rownames(.data)[i], id=row_header_id)
+      current_row_header = row_header_id
+    }
+
     row <- .data[i, ]
     for (j in seq_len(ncol(row))) {
-      headers <- sprintf("columnHead%i", j)
+      headers <- trimws(paste(sprintf(COL_ID_TEMPLATE, j), current_row_header))
       cell_value <- as.character(row[j])
       xml2::xml_add_child(xml_row, "td", cell_value, headers = headers)
     }
--- tests/testthat/test_table_generation.R
+++ tests/testthat/test_table_generation.R
@@ -1,12 +1,9 @@
 context("HTML table generation")
 
-stringify_expected <- function(expected_xml){
-  return(paste(xml2::xml_find_first(expected_xml, "//table")))
-}
 
 test_that("default arguments produce sensible output", {
   # Get the first 3 rows of `trees` and produce an HTML table from it
-  actual <- table508::create_508_table(trees[1:3, ])
+  actual <- create_508_table(trees[1:3, ])
 
   expected <- xml2::read_xml('
   <table id="table">
@@ -32,6 +29,52 @@ test_that("default arguments produce sensible output", {
         <td headers="columnHead1">8.8</td>
         <td headers="columnHead2">63</td>
         <td headers="columnHead3">10.2</td>
+      </tr>
+    </tbody>
+  </table>')
+  expect_equal(paste(actual), paste(xml2::xml_find_first(expected, "//table")))
+})
+
+test_that("error raised when rownames_as_headers set without top-left cell", {
+  expect_error(create_508_table(trees[1:3, ], rownames_as_headers = T),
+               'rownames_as_headers without setting top_left_cell')
+})
+
+test_that("row names used as row headers when argument set", {
+  # Get the first 3 rows of `trees` and produce an HTML table from it
+  data <- trees[1:3, ]
+  rownames(data) <- c("Tree 1", "Tree 2", "Tree 3")
+  actual <- create_508_table(data, rownames_as_headers = T,
+                             top_left_cell='Specimen')
+
+  expected <- xml2::read_xml('
+  <table id="table">
+    <thead>
+      <tr>
+        <th id="columnHead0">Specimen</th>
+        <th id="columnHead1">Girth</th>
+        <th id="columnHead2">Height</th>
+        <th id="columnHead3">Volume</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th id="rowHead1">Tree 1</th>
+        <td headers="columnHead1 rowHead1">8.3</td>
+        <td headers="columnHead2 rowHead1">70</td>
+        <td headers="columnHead3 rowHead1">10.3</td>
+      </tr>
+      <tr>
+        <th id="rowHead2">Tree 2</th>
+        <td headers="columnHead1 rowHead2">8.6</td>
+        <td headers="columnHead2 rowHead2">65</td>
+        <td headers="columnHead3 rowHead2">10.3</td>
+      </tr>
+      <tr>
+        <th id="rowHead3">Tree 3</th>
+        <td headers="columnHead1 rowHead3">8.8</td>
+        <td headers="columnHead2 rowHead3">63</td>
+        <td headers="columnHead3 rowHead3">10.2</td>
       </tr>
     </tbody>
   </table>')
