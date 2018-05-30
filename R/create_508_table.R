21df1c965f4cd123e7990db0598b486461e2a318
Zachary T Cross <zachary.t.cross@gmail.com>
2018-05-30 08:20:43 -0400

    Updated table generation to specifically satisfy 508 requirement `H6`, which specifies that tables with exactly 1 row header and 1 column header per cell use the `scope` attribute to enable accessibility.

--- R/create_508_table.R
+++ R/create_508_table.R
@@ -1,30 +1,31 @@
 #' Create a 508-compliant HTML table from tabular data
 #'
-#' \code{create_508_table} returns an HTML string representing a 508-compliant
-#' version of the tabular data \code{.data}.
+#' \code{create_508_table} returns an xml2 Node representing a 508-compliant
+#' version of the tabular data \code{.data}, formatted to qualify for
+#' requirement H6 of the [HHS section-508 compliance checklist](https://www.hhs.gov/web/section-508/making-files-accessible/checklist/html/index.html).
+#'
+#' Note that this package (currently) supports only tables with exactly one set
+#' of row name and one set of column names. Support for requirement H7, which
+#' defines how to make tables with nested column and row headers compliant, is
+#' not included.
+#'
 #' @param .data A data frame
 #' @param table_id A string representing the \code{id} attribute of the
 #'   resulting \code{<table>} node
-#' @param rownames_as_headers A boolean toggling whether to include row headers
-#'   based on the \code{rownames(.data)}
-#' @param top_left_cell A string indicating value of top-left cell; used when
-#'   rownames_as_headers if TRUE
+#' @param top_left_cell A string indicating value of top-left cell, defaults to
+#' the word `Row`
 #' @param summary_attr A string used as the table's summary-attribute
 #' @param caption A string used to create a \code{<caption>} tag
 #' @return xml2 node containing \code{<table>}
 #'
+#' @examples
+#' html_node <- table508::create_508_table(trees)
+#' writeLines(paste(html_node))
+#'
 #' @export
 create_508_table <- function(.data, table_id = "table",
-                             rownames_as_headers=FALSE, top_left_cell=NULL,
-                             summary_attr=NULL, caption=NULL){
-
-  # Validate arguments
-  if (rownames_as_headers && is.null(top_left_cell)){
-    stop("Can't use rownames_as_headers without setting top_left_cell as well")
-  }
-
-  COL_ID_TEMPLATE <- "columnHead%i"
-  ROW_ID_TEMPLATE <- "rowHead%i"
+                             top_left_cell="Row", summary_attr=NULL,
+                             caption=NULL){
 
   # Create the root xml node
   r <- xml2::xml_new_root("root")
@@ -45,33 +46,26 @@ create_508_table <- function(.data, table_id = "table",
   header <- xml2::xml_add_child(table, "thead")
   header_row <- xml2::xml_add_child(header, "tr")
 
-  if (rownames_as_headers){
-    xml2::xml_add_child(header_row, "th", top_left_cell,
-                        id = sprintf(COL_ID_TEMPLATE, 0))
-  }
+  # Add the top-left-cell (aka the column header for the row names)
+  xml2::xml_add_child(header_row, "th", top_left_cell,
+                      scope = "col")
   for (i in seq_along(colnames(.data))){
-    element_id <- sprintf(COL_ID_TEMPLATE, i)
-    xml2::xml_add_child(header_row, "th", colnames(.data)[i], id = element_id)
+    xml2::xml_add_child(header_row, "th", colnames(.data)[i], scope = "col")
   }
 
   # Create each data row
   body <- xml2::xml_add_child(table, "tbody")
 
-  current_row_header <- NULL
   for (i in seq_len(nrow(.data))){
     xml_row <- xml2::xml_add_child(body, "tr")
 
-    if (rownames_as_headers){
-      row_header_id <- sprintf(ROW_ID_TEMPLATE, i)
-      xml2::xml_add_child(xml_row, "th", rownames(.data)[i], id = row_header_id)
-      current_row_header <- row_header_id
-    }
+    # Add the <th> entry for this row
+    xml2::xml_add_child(xml_row, "th", rownames(.data)[i], scope = "row")
 
     row <- .data[i, ]
     for (j in seq_len(ncol(row))) {
-      headers <- trimws(paste(sprintf(COL_ID_TEMPLATE, j), current_row_header))
       cell_value <- as.character(row[j])
-      xml2::xml_add_child(xml_row, "td", cell_value, headers = headers)
+      xml2::xml_add_child(xml_row, "td", cell_value)
     }
   }
 
--- tests/testthat/test_table_generation.R
+++ tests/testthat/test_table_generation.R
@@ -9,72 +9,71 @@ test_that("default arguments produce sensible output", {
   <table id="table">
     <thead>
       <tr>
-        <th id="columnHead1">Girth</th>
-        <th id="columnHead2">Height</th>
-        <th id="columnHead3">Volume</th>
+        <th scope="col">Row</th>
+        <th scope="col">Girth</th>
+        <th scope="col">Height</th>
+        <th scope="col">Volume</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td headers="columnHead1">8.3</td>
-        <td headers="columnHead2">70</td>
-        <td headers="columnHead3">10.3</td>
+        <th scope="row">1</th>
+        <td>8.3</td>
+        <td>70</td>
+        <td>10.3</td>
       </tr>
       <tr>
-        <td headers="columnHead1">8.6</td>
-        <td headers="columnHead2">65</td>
-        <td headers="columnHead3">10.3</td>
+        <th scope="row">2</th>
+        <td>8.6</td>
+        <td>65</td>
+        <td>10.3</td>
       </tr>
       <tr>
-        <td headers="columnHead1">8.8</td>
-        <td headers="columnHead2">63</td>
-        <td headers="columnHead3">10.2</td>
+        <th scope="row">3</th>
+        <td>8.8</td>
+        <td>63</td>
+        <td>10.2</td>
       </tr>
     </tbody>
   </table>')
   expect_equal(paste(actual), paste(xml2::xml_find_first(expected, "//table")))
 })
 
-test_that("error raised when rownames_as_headers set without top-left cell", {
-  expect_error(create_508_table(trees[1:3, ], rownames_as_headers = T),
-               "rownames_as_headers without setting top_left_cell")
-})
 
-test_that("row names used as row headers when argument set", {
+test_that("row names used as row headers and top_left_cell arg respected", {
   # Get the first 3 rows of `trees` and produce an HTML table from it
   data <- trees[1:3, ]
   rownames(data) <- c("Tree 1", "Tree 2", "Tree 3")
-  actual <- create_508_table(data, rownames_as_headers = T,
-                             top_left_cell = "Specimen")
+  actual <- create_508_table(data, top_left_cell = "Specimen")
 
   expected <- xml2::read_xml('
   <table id="table">
     <thead>
       <tr>
-        <th id="columnHead0">Specimen</th>
-        <th id="columnHead1">Girth</th>
-        <th id="columnHead2">Height</th>
-        <th id="columnHead3">Volume</th>
+        <th scope="col">Specimen</th>
+        <th scope="col">Girth</th>
+        <th scope="col">Height</th>
+        <th scope="col">Volume</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <th id="rowHead1">Tree 1</th>
-        <td headers="columnHead1 rowHead1">8.3</td>
-        <td headers="columnHead2 rowHead1">70</td>
-        <td headers="columnHead3 rowHead1">10.3</td>
+        <th scope="row">Tree 1</th>
+        <td>8.3</td>
+        <td>70</td>
+        <td>10.3</td>
       </tr>
       <tr>
-        <th id="rowHead2">Tree 2</th>
-        <td headers="columnHead1 rowHead2">8.6</td>
-        <td headers="columnHead2 rowHead2">65</td>
-        <td headers="columnHead3 rowHead2">10.3</td>
+        <th scope="row">Tree 2</th>
+        <td>8.6</td>
+        <td>65</td>
+        <td>10.3</td>
       </tr>
       <tr>
-        <th id="rowHead3">Tree 3</th>
-        <td headers="columnHead1 rowHead3">8.8</td>
-        <td headers="columnHead2 rowHead3">63</td>
-        <td headers="columnHead3 rowHead3">10.2</td>
+        <th scope="row">Tree 3</th>
+        <td>8.8</td>
+        <td>63</td>
+        <td>10.2</td>
       </tr>
     </tbody>
   </table>')
@@ -90,16 +89,18 @@ test_that("summary keyword argument adds attribute to table tag", {
   <table id="table" summary="Some summary text">
     <thead>
       <tr>
-        <th id="columnHead1">Girth</th>
-        <th id="columnHead2">Height</th>
-        <th id="columnHead3">Volume</th>
+        <th scope="col">Row</th>
+        <th scope="col">Girth</th>
+        <th scope="col">Height</th>
+        <th scope="col">Volume</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td headers="columnHead1">8.3</td>
-        <td headers="columnHead2">70</td>
-        <td headers="columnHead3">10.3</td>
+        <th scope="row">1</th>
+        <td>8.3</td>
+        <td>70</td>
+        <td>10.3</td>
       </tr>
     </tbody>
   </table>')
@@ -115,16 +116,18 @@ test_that("caption keyword argument adds <caption> tag when string supplied", {
     <caption>Caption text</caption>
     <thead>
       <tr>
-        <th id="columnHead1">Girth</th>
-        <th id="columnHead2">Height</th>
-        <th id="columnHead3">Volume</th>
+        <th scope="col">Row</th>
+        <th scope="col">Girth</th>
+        <th scope="col">Height</th>
+        <th scope="col">Volume</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td headers="columnHead1">8.3</td>
-        <td headers="columnHead2">70</td>
-        <td headers="columnHead3">10.3</td>
+        <th scope="row">1</th>
+        <td>8.3</td>
+        <td>70</td>
+        <td>10.3</td>
       </tr>
     </tbody>
   </table>')
